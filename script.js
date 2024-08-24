f9c0e441e86faa3ed4339ebd6c98279dd7e85aa5
RaphaDev9 <raphael.miranda1998@gmail.com>
2024-08-24 20:16:00 -0300

    Ajuste em dados vazios e ajuste mobile

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-/* mascara input */
+// mascara input
 
 function mascaraInput(){
     $("#cnpj").inputmask("99.999.999/9999-99");
 }
 
-/* consulta CNPJ */
+// consulta CNPJ
 
 function getCNPJ(){
     let cnpj = $("#cnpj").val();
-    const numCNPJ = cnpj.replace(/\s+/g, "").replace(/\D/g, ""); /* regex para remover os espaços em branco e caracteres especiais */
+    const numCNPJ = cnpj.replace(/\s+/g, "").replace(/\D/g, ""); // regex para remover os espaços em branco e caracteres especiais
 
     if (numCNPJ.length !== 14){
         alert("Insira um CNPJ válido");
@@ -24,18 +24,18 @@ function consultaCNPJ(cnpj){
         method: "GET",
         success: function(dados) {
 
-            /* juntar dados do endereço */
+            // juntar dados do endereço
 
             let endereco = JSON.stringify(dados.descricao_tipo_de_logradouro + " " + dados.logradouro + ", " + dados.numero + " - " + dados.bairro + " - " + dados.municipio).replaceAll('"','');
 
-            $("input.tbl[name='nome']").val(JSON.stringify(dados.nome_fantasia).replaceAll('"',''));
-            $("input.tbl[name='social']").val(JSON.stringify(dados.razao_social).replaceAll('"',''));
-            $("input.tbl[name='abertura']").val(JSON.stringify(dados.data_inicio_atividade).replaceAll('"',''));
-            $("input.tbl[name='situacao']").val(JSON.stringify(dados.descricao_situacao_cadastral).replaceAll('"',''));
-            $("input.tbl[name='atividade']").val(JSON.stringify(dados.cnae_fiscal_descricao).replaceAll('"',''));
-            $("input.tbl[name='endereco']").val(endereco);
-            $("input.tbl[name='telefone']").val(JSON.stringify(dados.ddd_telefone_1).replaceAll('"',''));
-            $("input.tbl[name='email']").val(JSON.stringify(dados.email).replaceAll('"',''));
+            $("input.tbl[name='nome']").val(verificarDados(JSON.stringify(dados.nome_fantasia).replaceAll('"','')));
+            $("input.tbl[name='social']").val(verificarDados(JSON.stringify(dados.razao_social).replaceAll('"','')));
+            $("input.tbl[name='abertura']").val(verificarDados(JSON.stringify(dados.data_inicio_atividade).replaceAll('"','')));
+            $("input.tbl[name='situacao']").val(verificarDados(JSON.stringify(dados.descricao_situacao_cadastral).replaceAll('"','')));
+            $("input.tbl[name='atividade']").val(verificarDados(JSON.stringify(dados.cnae_fiscal_descricao).replaceAll('"','')));
+            $("input.tbl[name='endereco']").val(verificarDados(endereco));
+            $("input.tbl[name='telefone']").val(verificarDados(JSON.stringify(dados.ddd_telefone_1).replaceAll('"','')));
+            $("input.tbl[name='email']").val(verificarDados(JSON.stringify(dados.email).replaceAll('"','')));
 
         },
         error: function(xhr, status, error) {
@@ -44,9 +44,19 @@ function consultaCNPJ(cnpj){
     });
 }
 
+// verificar se os dados estão preenchidos no cadastro
+
+function verificarDados(dados){
+ if(dados == ""){
+    return "Dados não cadastrados"
+ } else{
+    return dados
+ }
+}
+
 $(document).ready(function(){
     mascaraInput();
 
-    /* clique no button */
+    // clique no botão
     $("#btnConsulta").click(getCNPJ);
 });
